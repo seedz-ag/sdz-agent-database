03e9a8a46f607cb0278ce012d87311f1dfcdf82e
Diogo Nunes <diogo.nunes>
2023-10-23 10:06:55 -0300

    mongodb

--- dist/database.js
+++ dist/database.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sdz_agent_database_firebird_1 = __importDefault(require("sdz-agent-database-firebird"));
 const sdz_agent_database_informix_1 = __importDefault(require("sdz-agent-database-informix"));
+const sdz_agent_database_mongodb_1 = __importDefault(require("sdz-agent-database-mongodb"));
 const sdz_agent_database_mssql_1 = __importDefault(require("sdz-agent-database-mssql"));
 const sdz_agent_database_mysql_1 = __importDefault(require("sdz-agent-database-mysql"));
 const sdz_agent_database_oracle_1 = __importDefault(require("sdz-agent-database-oracle"));
@@ -15,9 +16,10 @@ class Database {
             firebird: sdz_agent_database_firebird_1.default,
             informix: sdz_agent_database_informix_1.default,
             oracle: sdz_agent_database_oracle_1.default,
+            mongodb: sdz_agent_database_mongodb_1.default,
             mssql: sdz_agent_database_mssql_1.default,
             mysql: sdz_agent_database_mysql_1.default,
-            odbc: sdz_agent_database_odbc_1.default
+            odbc: sdz_agent_database_odbc_1.default,
         };
         this.config = config;
     }
